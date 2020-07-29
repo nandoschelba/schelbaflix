d5ac67a6065952ed091a86f205b323206997e499
Luiz Fernando Schelbauer <nandoschelba@gmail.com>
2020-07-29 18:10:06 -0300

    Atualização

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Logo from '../../assets/imagens/Logo.png';
 import '../Menu/Menu.css';
 import Button from '../Button';
@@ -6,12 +7,12 @@ import Button from '../Button';
 function Menu() {
     return (
        <nav className="Menu">
-           <a href="/">
+           <Link to="/">
                 <img className ="Logo" src={Logo} alt="SCHELBAFLIX"/>
 
-           </a>
+           </Link>
 
-           <Button as="a" className="ButtonLink" href="/">
+           <Button as={Link} className="ButtonLink" to="/cadastro/video">
                Novo vídeo
             </Button>
        </nav>
